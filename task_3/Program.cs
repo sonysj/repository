8eb31fbaebc358c3c3bb4b8f5ed367954b28204b
sonysj <softyr200500@gmail.com>
2024-10-02 15:02:25 +0500

    вариант 19

---
+++ task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace task_3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите четырехзначное число:");
+        int n = int.Parse(Console.ReadLine());
+        int n1 = n / 1000;
+        int n2 = (n / 100) % 10;
+        int n3 = (n / 10) % 10;
+        int n4 = n % 10;
+
+        int chislo = n1 * 1000 + n3 * 100 + n4 * 10 + n2 ;
+
+        Console.WriteLine(chislo);
+
+        Console.ReadKey();
+    }
+}
