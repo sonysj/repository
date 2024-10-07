8ffc27a38af7b08252e8ca1559ca36b54fc6b675
sonysj <softyr200500@gmail.com>
2024-10-07 14:07:41 +0500

    task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение x");
+            var x = double.Parse(Console.ReadLine());
+
+            var y = MyFunction(x);
+
+            Console.WriteLine("f(x)= " + y);
+
+            Console.ReadKey();
+        }
+
+        static double MyFunction(double x)
+        {
+            return (-Math.Pow(x, 3) + (1 / (Math.Pow(x, 2) + 1))) / (1 + (5 / (Math.Pow(x, 2) + x + 1)));
+        }
+    }
+}
