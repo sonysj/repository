ef97816b032865273fa4502dc48f0c762e2ef54d
sonysj <softyr200500@gmail.com>
2024-11-11 13:16:00 +0500

    8 задание, 19 вариант

---
+++ task_8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение аргумента функции");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"f({x}) = {MyFunction(x)}");
+
+            Console.ReadKey();
+        }
+
+        static double MyFunction(double x)
+        {
+            if (x < 2)
+                return x - 2;
+            else if (x <= 3)
+                return 3 - x;
+
+            return 0;
+        }
+    }
+}
