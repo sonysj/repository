a872efad2dccd60f5cef53fc7960a777bf156991
sonysj <softyr200500@gmail.com>
2024-12-06 21:34:15 +0500

    10.2 задание, 19 вариант

---
+++ task_10_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите натуральное число n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n < 2)
+        {
+            Console.WriteLine("Для вычисления произведения должно быть как минимум два числа");
+            Console.ReadKey();
+            return;
+        }
+
+        Console.Write("Введите a0: ");
+        double previous = double.Parse(Console.ReadLine());
+
+        double current;
+        double lastProduct;
+        string products = ""; 
+        int i = 1;
+
+        while (i <= n) 
+        {
+            Console.Write($"Введите a{i}: ");
+            current = double.Parse(Console.ReadLine());
+
+            lastProduct = previous * current;
+            products += lastProduct + ", "; 
+
+            previous = current;
+            i++;
+        }
+
+        Console.WriteLine($"Последовательность произведений соседних чисел: {products}");
+
+        Console.ReadKey();
+    }
+}
